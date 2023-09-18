80d2ba84e0f0d1c34ac2f7f22e2e2aea2a61047c
Pascal Klaiss <pascal.klaiss@smino.com>
2023-09-18 15:49:06 +0200

    lesson 8

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,15 +1,24 @@
 import {CalculatorService} from "./calculator.service";
 import {LoggerService} from "./logger.service";
+import createSpyObj = jasmine.createSpyObj;
 
 describe('CalculatorService', () => {
+  let calculator: CalculatorService,
+    loggerSpy: any;
+  beforeEach(() => {
+    console.log('calling before')
+    loggerSpy = createSpyObj('LoggerService', ['log'])
+    calculator = new CalculatorService(loggerSpy);
+  })
   it('should add two numbers', () => {
-    const calculator = new CalculatorService(new LoggerService());
     const result = calculator.add(2, 3);
-    expect(result).toBe(5)
+    expect(result)
+      .withContext('Unexpected Addition')
+      .toBe(5)
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1)
   });
 
   it('should subtract two numbers', () => {
-    const calculator = new CalculatorService(new LoggerService());
     const result = calculator.subtract(2, 3);
     expect(result)
       .withContext('Unexpected Subtraction')

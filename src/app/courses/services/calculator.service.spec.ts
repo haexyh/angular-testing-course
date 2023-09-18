f8589608ca8284238d4243f99a64e1114c6a465e
Pascal Klaiss <pascal.klaiss@smino.com>
2023-09-18 15:55:54 +0200

    lesson 10

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,14 +1,17 @@
 import {CalculatorService} from "./calculator.service";
 import {LoggerService} from "./logger.service";
 import createSpyObj = jasmine.createSpyObj;
+import {TestBed} from "@angular/core/testing";
 
 describe('CalculatorService', () => {
   let calculator: CalculatorService,
     loggerSpy: any;
   beforeEach(() => {
-    console.log('calling before')
     loggerSpy = createSpyObj('LoggerService', ['log'])
-    calculator = new CalculatorService(loggerSpy);
+    TestBed.configureTestingModule({
+      providers: [CalculatorService, {provide: LoggerService, useValue: loggerSpy}]
+    })
+    calculator = TestBed.inject(CalculatorService);
   })
   it('should add two numbers', () => {
     const result = calculator.add(2, 3);

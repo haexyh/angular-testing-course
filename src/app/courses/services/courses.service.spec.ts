bc8725ae9699c7a7271c015b657cb95e3880580f
Pascal Klaiss <pascal.klaiss@smino.com>
2023-09-19 07:17:18 +0200

    lesson 13

---
+++ src/app/courses/services/courses.service.spec.ts
@@ -0,0 +1,22 @@
+import {CoursesService} from "./courses.service";
+import {TestBed} from "@angular/core/testing";
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
+
+describe('CourseService', () => {
+  let coursesService: CoursesService, httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [CoursesService]
+    })
+    coursesService = TestBed.inject(CoursesService);
+    httpTestingController = TestBed.inject(HttpTestingController)
+  })
+
+  it('should retrieve all courses', () => {
+
+
+  });
+
+})

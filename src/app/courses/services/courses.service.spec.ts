eb462047bcb1f0cf2dc4e3fe7c496c54d8719bc1
Pascal Klaiss <pascal.klaiss@smino.com>
2023-09-19 08:06:21 +0200

    lesson 16

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -1,8 +1,9 @@
 import {CoursesService} from "./courses.service";
-import {TestBed} from "@angular/core/testing";
+import {TestBed, tick} from "@angular/core/testing";
 import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
 import {COURSES} from "../../../../server/db-data";
 import {Course} from "../model/course";
+import {HttpErrorResponse} from "@angular/common/http";
 
 describe('CourseService', () => {
   let coursesService: CoursesService, httpTestingController: HttpTestingController;
@@ -23,25 +24,20 @@ describe('CourseService', () => {
       expect(courses.length).withContext('incorrect number of courses').toBe(12)
       expect(courses.find(c => c.id == 12).titles.description).withContext('incorrect title').toBe('Angular Testing Course');
     })
-    const req = httpTestingController.expectOne('/api/courses')
-    expect(req.request.method).toEqual("GET");
+    const req = httpTestingController.expectOne({url: '/api/courses', method: 'GET'})
     req.flush({payload: Object.values(COURSES)})
-
   });
-  it('should find  a coourse by id', () => {
-    coursesService.findCourseById(12).subscribe(course => {
-      expect(course).toBeTruthy();
-      expect(course.id).toBe(12);
+
+  it('should find a course by id', () => {
+    coursesService.findCourseById(12).subscribe(c => {
+      expect(c).toBeTruthy();
     })
 
-    const req = httpTestingController.expectOne('/api/courses/12')
-    expect(req.request.method).toEqual("GET");
+    const req = httpTestingController.expectOne({method: 'GET', url: '/api/courses/12'})
     req.flush({payload: COURSES[12]})
-
   });
 
   it('should save the course data', () => {
-
     const changes: Partial<Course> = {titles: {description: 'Testing'}}
     coursesService.saveCourse(12, changes)
       .subscribe(course => {
@@ -49,10 +45,23 @@ describe('CourseService', () => {
         expect(course.id).toBe(12);
       })
 
-    const req = httpTestingController.expectOne('/api/courses/12')
-    expect(req.request.method).toBe("PUT");
+    const req = httpTestingController.expectOne({url: '/api/courses/12', method: 'PUT'})
     expect(req.request.body.titles.description).toEqual(changes.titles.description);
     req.flush({...COURSES[12], changes})
+  });
+
+  it('should throw an error if save course fails', () => {
+
+    const changes: Partial<Course> = {titles: {description: 'Testing'}}
+    coursesService.saveCourse(12, changes)
+      .subscribe({
+        next: _ => fail(), error: (e: HttpErrorResponse) =>
+          expect(e.status).toBe(500)
+      })
+
+    const req = httpTestingController.expectOne({url: '/api/courses/12', method: 'PUT'})
+    expect(req.request.body.titles.description).toEqual(changes.titles.description);
+    req.flush('Save course failed', {status: 500, statusText: 'Internal server error'})
   });
 
 

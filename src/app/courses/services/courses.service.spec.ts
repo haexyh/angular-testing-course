289cc4185bb5181f0b82f8829439f7070dc32bc5
Pascal Klaiss <pascal.klaiss@smino.com>
2023-09-19 07:29:26 +0200

    lesson 14

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -1,6 +1,7 @@
 import {CoursesService} from "./courses.service";
 import {TestBed} from "@angular/core/testing";
 import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
+import {COURSES} from "../../../../server/db-data";
 
 describe('CourseService', () => {
   let coursesService: CoursesService, httpTestingController: HttpTestingController;
@@ -16,6 +17,14 @@ describe('CourseService', () => {
 
   it('should retrieve all courses', () => {
 
+    coursesService.findAllCourses().subscribe(courses => {
+      expect(courses).withContext('no courses').toBeTruthy();
+      expect(courses.length).withContext('incorrect number of courses').toBe(12)
+      expect(courses.find(c => c.id == 12).titles.description).withContext('incorrect title').toBe('Angular Testing Course');
+    })
+    const req = httpTestingController.expectOne('/api/courses')
+    expect(req.request.method).toEqual("GET");
+    req.flush({payload: Object.values(COURSES)})
 
   });
 

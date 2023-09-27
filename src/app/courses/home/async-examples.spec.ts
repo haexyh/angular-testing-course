20084289913343862328c2161eb0589ee19c60a1
Pascal Klaiss <pascal.klaiss@smino.com>
2023-09-27 07:49:00 +0200

    lesson 30

--- src/app/courses/home/async-examples.spec.ts
+++ src/app/courses/home/async-examples.spec.ts
@@ -1,5 +1,7 @@
-fdescribe('async testing examples', () =>{
-  it('should show simple done function', (done) => {
+import {fakeAsync, flush, tick} from "@angular/core/testing";
+
+fdescribe('async testing examples', () => {
+  it('Async example with done', (done) => {
     let test = false;
     setTimeout(() => {
       test = true;
@@ -8,4 +10,24 @@ fdescribe('async testing examples', () =>{
     }, 1_000);
 
   });
+  it('Async example with tick', fakeAsync(() => {
+    let test = false;
+    setTimeout(() =>
+        test = true
+      , 1_000);
+    tick(1_000)
+    expect(test).toBeTruthy();
+  }));
+
+  it('Async example with flush', fakeAsync(() => {
+    let test = false;
+    setTimeout(() =>{});
+    setTimeout(() =>
+        test = true
+      , 1_000);
+    // clears all timeouts
+    flush();
+    expect(test).toBeTruthy();
+
+  }));
 })
